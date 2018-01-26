b23db92ccb9b1198bc6ec00498a0b9b67705b4c4
Brandon Fowler <bmfowler4@gmail.com>
2018-01-25 19:03:47 -1000

    displaying player

--- js/game.js
+++ js/game.js
@@ -2,15 +2,18 @@
   const GAME_WIDTH = 460;
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = `game`;
+  const GFX = `gfx`;
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
+  let player;
+  // Core game methods
   function preload() {
-    
+    game.load.spritesheet(GFX, `../assets/shmup-spritesheet-140x56-28x28-tile.png`, 28, 28);
   }
 
   function create() {
-
+    player = game.add.sprite(100, 100, GFX, 8);
   }
 
   function update() {
