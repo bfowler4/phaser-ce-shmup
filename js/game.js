db9a014e5e86cd56f5ffefa254abd4fcd9bb0e17
Brandon Fowler <bmfowler4@gmail.com>
2018-01-25 20:02:40 -1000

    game over if player hits ship with gameover screen

--- js/game.js
+++ js/game.js
@@ -6,7 +6,7 @@
   const INITIAL_MOVESPEED = 4;
   const SQRT_TWO = Math.sqrt(2);
   const PLAYER_BULLET_SPEED = 6;
-  const ENEMY_SPAWN_FREQ = 100; // higher is less frequent
+  const ENEMY_SPAWN_FREQ = 50; // higher is less frequent
   const ENEMY_SPEED = 4.5;
 
   const randomGenerator = new Phaser.RandomDataGenerator();
@@ -25,7 +25,7 @@
   function create() {
     cursors = game.input.keyboard.createCursorKeys();
     cursors.fire = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-    cursors.fire.onUp.add( handlePlayerFire );
+    cursors.fire.onUp.add(handlePlayerFire);
 
     player = game.add.sprite(100, 100, GFX, 8);
     player.moveSpeed = INITIAL_MOVESPEED;
@@ -69,54 +69,72 @@
       case cursors.up.isDown:
         player.y -= player.moveSpeed * movingH;
         break;
-    } 
+    }
   };
 
   function handlePlayerFire() {
-    playerBullets.add( game.add.sprite(player.x, player.y, GFX, 7));
+    playerBullets.add(game.add.sprite(player.x, player.y, GFX, 7));
   }
 
   function handleBulletAnimations() {
-    playerBullets.children.forEach( bullet => bullet.y -= PLAYER_BULLET_SPEED);
+    playerBullets.children.forEach(bullet => bullet.y -= PLAYER_BULLET_SPEED);
   }
 
   function handleEnemyActions() {
-    enemies.children.forEach( enemy => enemy.y += ENEMY_SPEED );
+    enemies.children.forEach(enemy => enemy.y += ENEMY_SPEED);
   };
 
   function handleCollisions() {
     // check if any bullets touch any enemies
     let enemiesHit = enemies.children
-      .filter( enemy => enemy.alive )
-      .filter( enemy => 
-        playerBullets.children.some( 
-          bullet => enemy.overlap(bullet) 
-        ) 
+      .filter(enemy => enemy.alive)
+      .filter(enemy =>
+        playerBullets.children.some(
+          bullet => enemy.overlap(bullet)
+        )
       );
 
-    if( enemiesHit.length ){
+    if (enemiesHit.length) {
       // clean up bullets that land
       playerBullets.children
-        .filter( bullet => bullet.overlap(enemies) )
-        .forEach( removeBullet );
+        .filter(bullet => bullet.overlap(enemies))
+        .forEach(removeBullet);
 
-      enemiesHit.forEach( destroyEnemy );
+      enemiesHit.forEach(destroyEnemy);
+    }
+
+    enemiesHit = enemies.children
+      .filter(enemy => enemy.overlap(player));
+
+    if (enemiesHit.length) {
+      handlePlayerHit();
+
+      enemiesHit.forEach(destroyEnemy);
     }
   };
 
+  function handlePlayerHit() {
+    gameOver();
+  }
+
   // Behavioral functions
   function randomlySpawnEnemy() {
-    if(randomGenerator.between(0, ENEMY_SPAWN_FREQ) === 0) {
+    if (randomGenerator.between(0, ENEMY_SPAWN_FREQ) === 0) {
       let randomX = randomGenerator.between(0, GAME_WIDTH);
-      enemies.add( game.add.sprite(randomX, -24, GFX, 0));
+      enemies.add(game.add.sprite(randomX, -24, GFX, 0));
     }
+  }
+
+  function gameOver() {
+    game.state.destroy();
+    game.add.text(90, 200, `YOUR HEAD ASPLODE`, { fill: '#FFFFFF' });
   }
 
   // Utlity functions
   function cleanup() {
     playerBullets.children
-      .filter( bullet => bullet.y < -14 )
-        .forEach( bullet => bullet.destroy() );
+      .filter(bullet => bullet.y < -14)
+      .forEach(bullet => bullet.destroy());
   }
 
   function removeBullet(bullet) {
