b0ef8c3a2ca833b4a3b28c3aece69a48cfb8011d
Brandon Fowler <bmfowler4@gmail.com>
2018-01-25 19:16:19 -1000

    added player movement

--- js/game.js
+++ js/game.js
@@ -3,21 +3,45 @@
   const GAME_HEIGHT = 600;
   const GAME_CONTAINER_ID = `game`;
   const GFX = `gfx`;
+  const INITIAL_MOVESPEED = 4;
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
   let player;
+  let cursors;
   // Core game methods
   function preload() {
     game.load.spritesheet(GFX, `../assets/shmup-spritesheet-140x56-28x28-tile.png`, 28, 28);
   }
 
   function create() {
+    cursors = game.input.keyboard.createCursorKeys();
     player = game.add.sprite(100, 100, GFX, 8);
+    player.moveSpeed = INITIAL_MOVESPEED;
   }
 
   function update() {
-
+    handlePlayerMovement();
   }
+
+  //handler function
+  function handlePlayerMovement() {
+    switch( true ){
+      case cursors.left.isDown:
+        player.x -= player.moveSpeed;
+        break;
+      case cursors.right.isDown:
+        player.x += player.moveSpeed;
+        break;
+    }
+    switch( true ){
+      case cursors.down.isDown:
+        player.y += player.moveSpeed;
+        break;
+      case cursors.up.isDown:
+        player.y -= player.moveSpeed;
+        break;
+    }
+  };
 
 })(window.Phaser);
